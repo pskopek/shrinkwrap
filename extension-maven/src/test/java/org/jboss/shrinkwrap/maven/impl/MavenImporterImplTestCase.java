3cace8308a49de7e0a23b78c9c960f5292bee422
Aslak Knutsen <aslak@redhat.com>
2010-07-26 23:03:27 +0200

    SHRINKWRAP-140 Added test Maven project to be used by unit test to have a controlled environment for the Maven Importer test

--- extension-maven/src/test/java/org/jboss/shrinkwrap/maven/impl/MavenImporterImplTestCase.java
+++ extension-maven/src/test/java/org/jboss/shrinkwrap/maven/impl/MavenImporterImplTestCase.java
@@ -17,11 +17,15 @@
 package org.jboss.shrinkwrap.maven.impl;
 
 import java.io.File;
+import java.util.logging.Logger;
 
+import org.jboss.shrinkwrap.api.ArchivePath;
+import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.formatter.Formatters;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.maven.api.MavenImporter;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -33,24 +37,22 @@ import org.junit.Test;
 public class MavenImporterImplTestCase
 {
 
+   private static Logger log = Logger.getLogger(MavenImporterImplTestCase.class.getName());
+   
    @Test
-   public void test() throws Exception 
-   {
-      for(int i = 0; i < 5; i++)
-      {
-         shouldBeAbleToImplementAMavenGeneratedArtifact();
-      }
-   }
    public void shouldBeAbleToImplementAMavenGeneratedArtifact() throws Exception
    {
+     
       long start = System.currentTimeMillis();
       JavaArchive jar = ShrinkWrap.create(JavaArchive.class)
                                     .as(MavenImporter.class)
-                                       .from(new File("/home/aslak/dev/source/testing/arquillian/junit/pom.xml"))
+                                       .from(new File("src/test/resources/test-project/pom.xml"))
                                     .as(JavaArchive.class);
      
-      System.out.println(jar.toString(Formatters.VERBOSE));
-      
-      System.out.println("build took: " + (System.currentTimeMillis() - start) + " ms");
+      log.info(jar.toString(Formatters.VERBOSE));
+      log.fine("build took: " + (System.currentTimeMillis() - start) + " ms");
+
+      ArchivePath testPath = ArchivePaths.create("org/jboss/shrinkwrap/maven/DummyClassForTesting.class");
+      Assert.assertTrue("Archive should contain class", jar.contains(testPath));
    }
 }
